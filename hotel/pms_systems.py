9ccb824358e02abd2de3e8b99abb06aa02d7f9c0
Nenad <nenadkrdzev@gmail.com>
2024-02-01 00:00:01 +0100

    Added handle_webhook logic,but need to fix phonenumbers module

--- hotel/pms_systems.py
+++ hotel/pms_systems.py
@@ -2,7 +2,8 @@ from abc import ABC, abstractmethod
 import inspect
 import sys
 import json
-from .models import Stay
+import phonenumbers
+from .models import Stay , Guest
 
 from typing import Optional
 
@@ -89,8 +90,60 @@ class PMS_Mews(PMS):
             return {}
 
     def handle_webhook(self, webhook_data: dict) -> bool:
+        #  """
+        # This method is called when we receive a webhook from the PMS.
+        # Handle webhook handles the events and updates relevant models in the database.
+        # Requirements:
+        #     - Now that the PMS has notified you about an update of a reservation, you need to
+        #         get more details of this reservation. For this, you can use the mock API
+        #         call get_reservation_details(reservation_id).
+        #     - Handle the payload for the correct hotel.
+        #     - Update or create a Stay.
+        #     - Update or create Guest details.
+        # """
         # TODO: Implement the method
+        
+        #Retrieve the id for the hotel and mapping it 
+        id_hotel=webhook_data["HotelId"]
+        try:
+            #finding the hotel by Id
+            hotel=Hotel.objects.get(pms_hotel_id=id_hotel)
+            #Event cycle for the json in order to retrieve the reservation and guest details
+            events=webhook_data["Events"]
+            for event in events:
+                res_id=event["Value"]["ReservationId"]
+                res_details=self.clean_webhook_payload(get_reservation_details(res_id))
+                guest_id=res_details["GuestId"]
+                guest_details=self.clean_webhook_payload(get_guest_details(guest_id))
 
+                #Updating or create a new stay with its respected fields
+                stay, created = Stay.objects.update_or_create(
+                            hotel=hotel,
+                            pms_reservation_id=res_id,
+                            pms_guest_id=guest_id,
+                            defaults={
+                                'checkin': res_details["CheckInDate"],
+                                'checkout': res_details["CheckOutDate"],
+                                'status': res_details["Status"],
+                            }
+                    )
+                
+                #Using the phonenumbers module to validate the phone number
+                phone=guest_details["Phone"]
+                validate_phone_number(phone)
+
+                #Updating or create a new guest with its respected fields
+                guest, created = Guest.objects.update_or_create(
+                        phone=phone,
+                        defaults={
+                            'name':guest_details["Name"],
+                        }
+                    )
+                #Saving the changes
+                stay.guest=guest
+                stay.save()
+        except APIError as err:
+            print(f"Api error at {err}") 
         return True
 
     def update_tomorrows_stays(self) -> bool:
@@ -112,3 +165,13 @@ def get_pms(name):
 
     # if we have a PMS class for the given name, return an instance of it
     return getattr(current_module, fullname)() if fullname in clsnames else False
+
+#Installed a module in order to validate the phone number
+def validate_phone_number(phone_number):
+    try:
+        parsed_number=phonenumbers.parse(phone_number,None)
+
+        is_valid = phonenumbers.is_valid_number(parsed_number)
+        return is_valid
+    except phonenumbers.NumberParseException:
+        return False
